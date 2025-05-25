9e2995349eafeedc1a90f5e12297d05a290feee5
kamikaonashi <thorschi93@gmx.at>
2025-05-25 14:31:02 +0200

    veux: parts: force HDR and HLG
    based on the idea of
    https://github.com/Chaitanyakm/device_xiaomi_sm8450-common/commit/3af1b7db9711ba96adaed6265392d1f9c69d1cd9
    and
    https://github.com/Chaitanyakm/device_xiaomi_sm8450-common/commit/ef0012adbfb84f73ed36b7a2598db1ebf170a17b

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,7 +20,15 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.display.DisplayManager;
+import android.os.IBinder;
 import android.util.Log;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import androidx.preference.PreferenceManager;
+import android.view.Display;
+import android.view.Display.HdrCapabilities;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -36,6 +44,14 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Received boot completed intent");
         DozeUtils.onBootCompleted(context);
         ThermalUtils.startService(context);
-        RefreshUtils.startService(context);        
+        RefreshUtils.startService(context);
+        overrideHdrTypes(context);
     }
+
+    private static void overrideHdrTypes(Context context) {
+        // Override HDR
+        final DisplayManager dm = context.getSystemService(DisplayManager.class);
+        dm.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
+                HdrCapabilities.HDR_TYPE_HDR10, HdrCapabilities.HDR_TYPE_HDR10_PLUS, HdrCapabilities.HDR_TYPE_HLG});
+        }
 }
