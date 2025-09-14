5d13bbaede4ad194d69470814503119c7bfef73d
Bruno Martins <bgcngm@gmail.com>
2025-09-14 21:37:49 +0100

    veux: Rename display config file locally
    Change-Id: Ia8c31aaf2e281c8ac50b1c4008ba623d84484d04
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -105,7 +105,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/display_id_4630947081618265473.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947081618265473.xml
+    $(LOCAL_PATH)/configs/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630947081618265473.xml
 
 # DRM
 PRODUCT_PACKAGES += \
