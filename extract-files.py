ed5033bf3f6054ec293e9cce851f194b24f02b02
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    veux: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb
    Signed-off-by: Frost <frost.github@proton.me>

--- extract-files.py
+++ extract-files.py
@@ -95,6 +95,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/init/wfdservice.rc': blob_fixup()
         .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so')
         .remove_needed('android.hidl.base@1.0.so'),
     'vendor/etc/camera/camxoverridesettings.txt': blob_fixup()
         .regex_replace('0x10080', '0')
