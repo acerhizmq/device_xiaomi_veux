b48f6c845d1fb2de7c9666dc6c4100c43acc9c73
Michael Bestas <mkbestas@lineageos.org>
2025-11-28 13:13:53 +0200

    veux: Remove Dialer overlay
    Call recording is now enabled by default
    
    Change-Id: Ie454647046b9a2e4b2129e3f04352dd08073c744
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -230,7 +230,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCOMMON \
-    DialerOverlayCOMMON \
     NexusLauncherOverlayCOMMON \
     TelephonyOverlayCOMMON
 
