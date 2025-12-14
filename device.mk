6d9a09179b6a00462a5379a8f9d6ca512f4caf7a
basamaryan <basam.aryan@gmail.com>
2025-12-13 18:02:38 -0800

    veux: overlay: Reduce blur radius
    Change-Id: Id6339df82156099ca14da684f236931b3ecf51ce
    Signed-off-by: Frost <frost.github@proton.me>

--- device.mk
+++ device.mk
@@ -231,6 +231,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayCOMMON \
     DialerOverlayCOMMON \
+    NexusLauncherOverlayCOMMON \
     TelephonyOverlayCOMMON
 
 PRODUCT_PACKAGES += \
