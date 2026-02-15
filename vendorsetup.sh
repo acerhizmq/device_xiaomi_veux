cd733b5c7fb9dbbc468b3fc24ef0580b542b42de
NoCache-69 <chethangowdasj86@gmail.com>
2026-02-15 18:41:09 +0530

    veux: Switch to StarryKernel
    Signed-off-by: NoCache-69 <chethangowdasj86@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 git clone https://github.com/Project-Astraverse/vendor_xiaomi_veux.git vendor/xiaomi/veux
-git clone https://github.com/Siarrrrrrrr/VEUX-Positron-Resukisu-Susfs.git kernel/xiaomi/sm6375
+git clone https://github.com/Starrykernel/kernel_xiaomi_veux.git -b 16-Q2-KSU kernel/xiaomi/sm6375 --depth=1
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
