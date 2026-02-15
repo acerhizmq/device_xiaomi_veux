000a4649f56cdc7f305f1563f8d21455c8a80170
NoCache-69 <chethangowdasj86@gmail.com>
2026-02-15 11:01:18 +0000

    veux: Init vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Project-Astraverse/vendor_xiaomi_veux.git vendor/xiaomi/veux
+git clone https://github.com/Siarrrrrrrr/VEUX-Positron-Resukisu-Susfs.git kernel/xiaomi/sm6375
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
