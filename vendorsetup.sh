fc5b7810ea4111afc5dc1d253bc11210a3b3c256
Acer <140119937+acerhizmq@users.noreply.github.com>
2026-02-16 21:48:36 +0300

    Remove sign keys cloning from vendorsetup.sh
    Removed cloning of sign keys repository from vendorsetup.sh.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,8 +8,5 @@ git clone https://github.com/AxionAOSP/android_packages_apps_ViPER4AndroidFX.git
 # MiuiCamera
 git clone https://github.com/frost-testzone/vendor_xiaomi_miuicamera-veux.git vendor/xiaomi/miuicamera-veux
 
-# Sign keys
-git clone https://github.com/ProjectInfinity-X/vendor_infinity-priv_keys vendor/infinity-priv/keys
-
 # Dolby
 git clone https://github.com/oscaro-resources/hardware_dolby.git hardware/dolby
