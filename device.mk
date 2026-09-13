3f7bccf0abf52d5039bd884024ce2f0b581cb55e
acerhizmq <acerhizm@protonmail.com>
2026-09-13 12:33:09 +0300

    veux: vintf: Add vendor.xiaomi.hardware.misys to framework compatibility matrix

--- device.mk
+++ device.mk
@@ -447,7 +447,8 @@ PRODUCT_COPY_FILES += \
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    $(LOCAL_PATH)/vintf/framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     device/xiaomi/veux/vintf/manifest.xml
